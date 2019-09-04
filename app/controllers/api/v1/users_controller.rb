59210de79b58b91a2d676f97074d617ad549f329
github username <lawrence@sciencedisrupt.com>
2019-09-04 16:56:27 +0100

    added method to slugify user url

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,6 +21,11 @@ class Api::V1::UsersController < ApplicationController
         render( json: Api::V1::UserSerializer.new(@user)  )
     end
 
+    def slug_show 
+        @user = User.find_by_slug(params[:slug])
+        render( json: Api::V1::UserSerializer.new(@user)  )
+    end
+
     def profile
         @user = User.find(params[:id])
         render( json: Api::V1::UserSerializer.new(@user)  )
--- app/serializers/api/v1/paper_serializer.rb
+++ app/serializers/api/v1/paper_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::PaperSerializer 
     include FastJsonapi::ObjectSerializer
-    attributes :id, :title, :abstract, :category, :doi, :rating, :user, :reviews
+    attributes :id, :title, :abstract, :category, :doi, :rating, :created_at, :user, :reviews
 end
