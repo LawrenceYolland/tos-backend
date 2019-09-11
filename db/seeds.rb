8d7ff48bd673d7fc936010fd9810c8c9bab2f190
github username <lawrence@sciencedisrupt.com>
2019-09-11 17:26:35 +0100

    new seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,70 +7,72 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # create test user
-# User.create!([
-#     { 
-#     username: "lawrence",
-#     password: "123",
-#     usertype: true,
-#     bio: Faker::Movies::Ghostbusters.quote 
-#     }
-# ])
+User.create!([
+    { 
+    username: "lawrence",
+    password: "123",
+    usertype: true,
+    bio: Faker::Movies::Ghostbusters.quote 
+    }
+])
 
-# 10.times do 
-#     Paper.create({
-#         title: Faker::Hipster.sentence,
-#         abstract: Faker::Hipster.paragraph(sentence_count: 3),
-#         category: ["Biology", "Physics", "Chemistry"][rand(3)],
-#         doi: "10.1016/j.cell.2015.02.015",
-#         user_id: 1
-#     })
-# end 
+10.times do 
+    Paper.create({
+        title: Faker::Hipster.sentence,
+        abstract: Faker::Hipster.paragraph(sentence_count: 3),
+        category: ["Biology", "Physics", "Chemistry"][rand(3)],
+        doi: "10.1016/j.cell.2015.02.015",
+        user_id: 1
+    })
+end 
 
-# 5.times do 
-#     Review.create({
-#         content: Faker::Hipster.paragraph(sentence_count: 2),
-#         paper_id: rand(10)+1,
-#         user_id: 1
-#     })
-# end 
+5.times do 
+    Review.create({
+        content: Faker::Hipster.paragraph(sentence_count: 2),
+        paper_id: rand(10)+1,
+        user_id: 1
+    })
+end 
 
 # create bank of researcher type users
-# 10.times do 
-#     User.create({
-#         username: Faker::Name.name,
-#         password: "123",
-#         usertype: true,
-#         bio: Faker::Movies::Ghostbusters.quote
-#     })
-# end
+10.times do 
+    User.create({
+        username: Faker::Name.name,
+        password: "123",
+        usertype: true,
+        bio: Faker::Movies::Ghostbusters.quote
+    })
+end
 
 # assign researchers some papers
-# 5.times do 
-#     Paper.create({
-#         title: Faker::Hipster.sentence,
-#         abstract: Faker::Hipster.paragraph(sentence_count: 3),
-#         category: ["Biology", "Physics", "Chemistry"][rand(3)],
-#         doi: "10.1016/j.cell.2015.02.015",
-#         user_id: rand(8)+2,
-#         rating: rand(5)
-#     })
-# end 
+10.times do 
+    Paper.create({
+        title: Faker::Hipster.sentence,
+        abstract: Faker::Hipster.paragraph(sentence_count: 3),
+        category: ["Biology", "Physics", "Chemistry"][rand(3)],
+        doi: "10.1016/j.cell.2015.02.015",
+        user_id: rand(8)+2,
+        rating: rand(5)
+    })
+end 
 
 # create some peers
-# 10.times do 
-#     User.create({
-#         username: Faker::Name.name,
-#         password: "123",
-#         usertype: false,
-#         bio: Faker::Movies::Ghostbusters.quote
-#     })
-# end
+10.times do 
+    User.create({
+        username: Faker::Name.name,
+        password: "123",
+        usertype: false,
+        bio: Faker::Movies::Ghostbusters.quote
+    })
+end
 
 # randomly assign users some reviews
-# 20.times do 
-#     Review.create({
-#         content: Faker::Hipster.paragraph(sentence_count: 2),
-#         paper_id: rand(18)+2,
-#         user_id: rand(18)+2
-#     })
-# end 
+20.times do 
+    Review.create({
+        content: Faker::Hipster.paragraph(sentence_count: 2),
+        paper_id: rand(18)+2,
+        user_id: rand(18)+2
+    })
+end 
+
+puts "---- SEEDED ----"
